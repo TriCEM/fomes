ee5ab52d21dc4081cf387afb3d63dde785f29e40
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-02-26 14:55:49 -0500

    fix trad model and update test for better beta

--- tests/testthat/test-mainruns.R
+++ tests/testthat/test-mainruns.R
@@ -1,10 +1,10 @@
 test_that("main model runs", {
 
   out <- sim_Gillespie_SIR(Iseed = 1, N = 1e3,
-                    beta = rep(1, 1e3),
+                    beta = rep(0.5, 1e3),
                     dur_I = 5,
                     rho = 5,
-                    initNC = 3,
+                    initNC = 5,
                     term_time = 500)
 
   testthat::expect_equal(length(out), 6)
@@ -12,7 +12,7 @@ test_that("main model runs", {
 
   # traditional model for internal checks and comparisons
  out <- fomes:::tradsim_Gillespie_SIR(Iseed = 1, N = 10,
-                        beta = 1,
+                        beta = 0.5,
                         dur_I = 1,
                         term_time = 500)
  testthat::expect_type(out, "list")
